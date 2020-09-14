bc435f6d28717750aba82e7c2f604c7be8509c14
李翰博 <xatian23@126.com>
2020-09-14 11:21:45 +0800

    more texts for raytracer.

--- Nix/Nix/NXRayTracer.cpp
+++ Nix/Nix/NXRayTracer.cpp
@@ -5,6 +5,7 @@
 #include "NXPMIntegrator.h"
 #include "NXPMSplitIntegrator.h"
 #include "NXSPPMIntegrator.h"
+#include "NXTimer.h"
 
 NXRayTracer::NXRayTracer()
 {
@@ -12,25 +13,31 @@ NXRayTracer::NXRayTracer()
 
 void NXRayTracer::RenderImage(const std::shared_ptr<NXScene>& pScene, NXRayTraceRenderMode rayTraceMode, bool bCenterRayTest)
 {
+	NXTimer t1, t2;
+	t1.Tick();
+
 	XMINT2 renderResolution = XMINT2(800, 600);
-	int pixelSample = 4;
+	int pixelSample = 16;
 
 	switch (rayTraceMode)
 	{
 	case NXRayTraceRenderMode::DirectLighting:
 	{
+		printf("DirectLighting Integrator Running...\n");
 		std::make_unique<NXDirectIntegrator>(renderResolution, pixelSample, "D:\\Nix_DirectLighting.bmp")->Render(pScene);
 		//std::make_unique<NXDirectIntegrator>(renderResolution, pixelSample, "D:\\Nix_DirectLighting.bmp")->CenterRayTest(pScene);
 		break;
 	}
 	case NXRayTraceRenderMode::PathTracing:
 	{
-		int pixelSample = 1;
+		printf("PathTracing Integrator Running...\n");
+		int pixelSample = 64;
 		std::make_unique<NXPathIntegrator>(renderResolution, pixelSample, "D:\\Nix_PathTracing.bmp")->Render(pScene);
 		break;
 	}
 	case NXRayTraceRenderMode::PhotonMapping:
 	{
+		printf("PhotonMapping Integrator Running...\n");
 		int nPhotons = 100000;
 		int nEstimatePhotons = 500;
 		std::make_unique<NXPMIntegrator>(renderResolution, pixelSample, "D:\\Nix_PhotonMapping.bmp", nPhotons, nEstimatePhotons)->Render(pScene);
@@ -38,6 +45,7 @@ void NXRayTracer::RenderImage(const std::shared_ptr<NXScene>& pScene, NXRayTrace
 	}
 	case NXRayTraceRenderMode::IrradianceCache:
 	{
+		printf("IrradianceCache Integrator Running...\n");
 		int nCausticPhotons = 50000;
 		int nGlobalPhotons = 200000;
 		std::make_unique<NXPMSplitIntegrator>(renderResolution, pixelSample, "D:\\Nix_PhotonMappingSplit.bmp", nCausticPhotons, nGlobalPhotons)->Render(pScene);
@@ -45,6 +53,7 @@ void NXRayTracer::RenderImage(const std::shared_ptr<NXScene>& pScene, NXRayTrace
 	}
 	case NXRayTraceRenderMode::SPPM:
 	{
+		printf("SPPM Integrator Running...\n");
 		int nPhotons = 200000;
 		std::make_unique<NXSPPMIntegrator>(renderResolution,  "D:\\Nix_SPPM.bmp", nPhotons)->Render(pScene);
 		break;
@@ -52,4 +61,8 @@ void NXRayTracer::RenderImage(const std::shared_ptr<NXScene>& pScene, NXRayTrace
 	default:
 		break;
 	}
+	t2.Tick();
+
+	float timeCost = (float)(t2.GetTimeDelta() - t1.GetTimeDelta());
+	printf("%f\n", timeCost * 1e-6f);
 }
