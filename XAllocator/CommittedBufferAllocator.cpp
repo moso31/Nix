0b649db709f0ddf3868f3a66ab575bd78ae7aad3
moso31 <xatian23@126.com>
2025-08-09 14:41:34 +0800

    ..

--- XAllocator/CommittedBufferAllocator.cpp
+++ XAllocator/CommittedBufferAllocator.cpp
@@ -2,12 +2,17 @@
 
 using namespace ccmem;
 
-CommittedBufferAllocator::CommittedBufferAllocator(ID3D12Device* pDevice, bool cpuAccessable, uint32_t pageBlockByteSize, uint32_t pageFullByteSize) :
+CommittedBufferAllocator::CommittedBufferAllocator(ID3D12Device* pDevice, bool cpuAccessable, bool isReadBack, uint32_t pageBlockByteSize, uint32_t pageFullByteSize) :
 	m_cpuAccessable(cpuAccessable),
+	m_isReadBack(isReadBack),
 	m_pageFullByteSize(pageFullByteSize),
 	m_pDevice(pDevice),
 	BuddyAllocator(pageBlockByteSize, pageFullByteSize)
 {
+	if (m_isReadBack && pageBlockByteSize < 512u)
+	{
+		assert(false);
+	}
 }
 
 void CommittedBufferAllocator::Alloc(uint32_t byteSize, const std::function<void(const CommittedBufferAllocTaskResult&)>& callback)
@@ -15,8 +20,17 @@ void CommittedBufferAllocator::Alloc(uint32_t byteSize, const std::function<void
 	BuddyAllocator::AddAllocTask(byteSize, nullptr, 0, [this, callback](const BuddyTaskResult& taskResult) {
 		const auto& memData = taskResult.memData;
 		CommittedBufferAllocTaskResult result;
-		result.cpuAddress = m_cpuAccessable ? m_allocatorMap[memData.pAllocator].m_pResourceData + memData.byteOffset : nullptr; // 如果buffer CPU不可读，返回nullptr!
-		result.gpuAddress = m_allocatorMap[memData.pAllocator].m_pResource->GetGPUVirtualAddress() + memData.byteOffset; 
+
+		if (m_cpuAccessable || m_isReadBack)
+			result.cpuAddress = m_allocatorMap[memData.pAllocator].m_pResourceData + memData.byteOffset;
+		else
+			result.cpuAddress = nullptr; 
+
+		if (m_isReadBack)
+			result.gpuAddress = NULL;
+		else 
+			result.gpuAddress = m_allocatorMap[memData.pAllocator].m_pResource->GetGPUVirtualAddress() + memData.byteOffset;
+
 		result.memData = memData;
 		callback(result);
 	});
@@ -30,23 +44,13 @@ void ccmem::CommittedBufferAllocator::Free(const XBuddyTaskMemData& memData)
 
 void ccmem::CommittedBufferAllocator::OnAllocatorAdded(BuddyAllocatorPage* pAllocator)
 {
-	D3D12_HEAP_PROPERTIES uploadHeapProps;
-	uploadHeapProps.Type = D3D12_HEAP_TYPE_UPLOAD;
-	uploadHeapProps.CPUPageProperty = D3D12_CPU_PAGE_PROPERTY_UNKNOWN;
-	uploadHeapProps.MemoryPoolPreference = D3D12_MEMORY_POOL_UNKNOWN;
-	uploadHeapProps.CreationNodeMask = 1;
-	uploadHeapProps.VisibleNodeMask = 1;
-
-	D3D12_HEAP_PROPERTIES defaultHeapProps;
-	defaultHeapProps.Type = D3D12_HEAP_TYPE_DEFAULT;
-	defaultHeapProps.CPUPageProperty = D3D12_CPU_PAGE_PROPERTY_UNKNOWN;
-	defaultHeapProps.MemoryPoolPreference = D3D12_MEMORY_POOL_UNKNOWN;
-	defaultHeapProps.CreationNodeMask = 1;
-	defaultHeapProps.VisibleNodeMask = 1;
+	D3D12_HEAP_PROPERTIES props{};
+	props.Type = m_isReadBack ? D3D12_HEAP_TYPE_READBACK : 
+		m_cpuAccessable ? D3D12_HEAP_TYPE_UPLOAD: D3D12_HEAP_TYPE_DEFAULT;
 
 	D3D12_RESOURCE_DESC cbDesc;
 	cbDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
-	cbDesc.Alignment = D3D12_DEFAULT_RESOURCE_PLACEMENT_ALIGNMENT;
+	cbDesc.Alignment = 0; 
 	cbDesc.Width = m_pageFullByteSize;
 	cbDesc.Height = 1; 
 	cbDesc.DepthOrArraySize = 1;
@@ -57,18 +61,20 @@ void ccmem::CommittedBufferAllocator::OnAllocatorAdded(BuddyAllocatorPage* pAllo
 	cbDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR; 
 	cbDesc.Flags = D3D12_RESOURCE_FLAG_NONE;
 
-	AllocatorData& newData = m_allocatorMap[pAllocator];
+	D3D12_RESOURCE_STATES initState = m_isReadBack ? D3D12_RESOURCE_STATE_COPY_DEST :
+		m_cpuAccessable ? D3D12_RESOURCE_STATE_GENERIC_READ : D3D12_RESOURCE_STATE_COMMON;
 
+	AllocatorData& newData = m_allocatorMap[pAllocator];
 	HRESULT hr = m_pDevice->CreateCommittedResource(
-		m_cpuAccessable ? &uploadHeapProps : &defaultHeapProps,
+		&props,
 		D3D12_HEAP_FLAG_NONE,
 		&cbDesc, // 资源描述
-		m_cpuAccessable ? D3D12_RESOURCE_STATE_GENERIC_READ : D3D12_RESOURCE_STATE_COMMON,
+		initState,
 		nullptr,
 		IID_PPV_ARGS(&newData.m_pResource)
 	);
 
-	if (m_cpuAccessable)
+	if (m_cpuAccessable || m_isReadBack)
 	{
 		// 如果cpu可访问，构建映射地址
 		newData.m_pResource->Map(0, nullptr, reinterpret_cast<void**>(&newData.m_pResourceData));
--- XAllocator/CommittedBufferAllocator.h
+++ XAllocator/CommittedBufferAllocator.h
@@ -22,7 +22,7 @@ namespace ccmem
 	public:
 		// pageBlockByteSize = 单个内存块的大小 pageFullByteSize = 单个Allocator内存大小. 要求必须是2的n次幂
 		// 这里约定单个内存块大小是64B，总内存大小是2GB
-		CommittedBufferAllocator(ID3D12Device* pDevice, bool cpuAccessable, uint32_t pageBlockByteSize = 64, uint32_t pageFullByteSize = 2147483648);
+		CommittedBufferAllocator(ID3D12Device* pDevice, bool cpuAccessable, bool isReadBack, uint32_t pageBlockByteSize = 64, uint32_t pageFullByteSize = 2147483648);
 		virtual ~CommittedBufferAllocator() {};
 
 		// 分配内存。结果是延迟生成的，需要在回调函数中获取分配的内存地址
@@ -44,6 +44,7 @@ namespace ccmem
 		ID3D12Device* m_pDevice;
 		std::unordered_map<BuddyAllocatorPage*, AllocatorData> m_allocatorMap;
 
+		bool m_isReadBack;
 		bool m_cpuAccessable; // 是否允许CPU读
 		uint32_t m_pageFullByteSize; // 每个Allocator的总内存大小
 	};
